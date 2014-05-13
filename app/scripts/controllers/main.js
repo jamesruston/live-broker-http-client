35a8eec71d2510a788465f43385e005fd1aae3de
James Ruston <jruston90@gmail.com>
2014-05-13 13:38:00 +0100

    Added a request builder directive. Updated url-bar directive.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,79 @@
 
 angular.module('liveBrokerHttpClientApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.environments = [
-      'http://pal.sandbox.dev.bbc.co.uk:8360/live-broker',
-      'http://api.int.bbc.co.uk/live-broker',
-      'http://api.test.bbc.co.uk/live-broker',
-      'http://api.stage.bbc.co.uk/live-broker',
-      'http://api.bbc.co.uk/live-broker'
-    ];
+
+    $scope.environments = {
+      sandbox: 'http://pal.sandbox.dev.bbc.co.uk:8360/live-broker',
+      int: 'http://api.int.bbc.co.uk/live-broker',
+      test: 'http://api.test.bbc.co.uk/live-broker',
+      stage: 'http://api.stage.bbc.co.uk/live-broker',
+      live: 'http://api.bbc.co.uk/live-broker'
+    };
+
+    $scope.url = {
+      base: "",
+      requests: []
+    }
+
+    $scope.services = [
+      {
+        "service": "Eavis",
+        "types": [
+          {
+            "title": "Set List",
+            "endpoint": "setlist",
+            "parameters": [
+              {
+                "name": "eventId",
+                "required": true,
+                "description": "The ID associated with the event"
+              },
+              {
+                "name": "coverage",
+                "required": true,
+                "description": "The ID for the requested coverage"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "service": "Storify",
+        "types": [
+          {
+            "title": "Story",
+            "endpoint": "storify",
+            "parameters": [
+              {
+                "name": "username",
+                "required": true,
+                "description": "The username the story belongs to."
+              },
+              {
+                "name": "story",
+                "required": true,
+                "description": "The name of the story."
+              }
+            ]
+          }
+        ]
+      }
+    ]
+
+    $scope.addRequest = function() {
+      var requestCount = $scope.url.requests.length;
+
+      var request = $scope.request(requestCount);
+      $scope.url.requests.push(request);
+    };
+
+    $scope.request = function(id) {
+      return {
+        id: id,
+        request: ""
+      }
+    }
+
+
+
   });
--- app/scripts/directives/urlbar.js
+++ app/scripts/directives/urlbar.js
@@ -3,17 +3,12 @@
 angular.module('liveBrokerHttpClientApp')
 .directive('urlBar', function () {
     return {
-      template: '<div><input class="urlbar" type="text"><div class="btn-group"><button class="btn btn-default">Visit</button><button class="btn btn-default">Copy</button></div></div>',
+      templateUrl: 'views/url-bar.html',
       restrict: 'E',
       replace: true,
       link: function postLink(scope, element, attrs) {
-          console.log(scope.environments);
-          $(element).find('.urlbar').on('change paste', urlBarChanged);
 
-          function urlBarChanged(event) {
-            var currentUrl = event.target.val;
-            console.log(scope.selectedEnvironment);
-          }
+
       }
     };
   });
